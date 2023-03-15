254af2cbbe8ae103994d77ad2fbb6e9c3446ae15
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-03-15 14:27:02 +0100

    Resolution of the exercice

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,90 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Tests
+        double width, height, areaPerBucket, area;
+        int extraBucket, expectedResult;
+
+        width = -3.4;
+        height = 2.1;
+        areaPerBucket = 1.5;
+        extraBucket = 2;
+        expectedResult = -1;
+        System.out.println("getBucketCount(" + width + ", " + height + ", " + areaPerBucket + ", " + extraBucket
+                + ") = " + PaintJob.getBucketCount(width, height, areaPerBucket, extraBucket) + " (expected: "
+                + expectedResult + ")");
+
+        width = 3.4;
+        height = 2.1;
+        areaPerBucket = 1.5;
+        extraBucket = 2;
+        expectedResult = 3;
+        System.out.println("getBucketCount(" + width + ", " + height + ", " + areaPerBucket + ", " + extraBucket
+                + ") = " + PaintJob.getBucketCount(width, height, areaPerBucket, extraBucket) + " (expected: "
+                + expectedResult + ")");
+
+        width = 2.75;
+        height = 3.25;
+        areaPerBucket = 2.5;
+        extraBucket = 1;
+        expectedResult = 3;
+        System.out.println("getBucketCount(" + width + ", " + height + ", " + areaPerBucket + ", " + extraBucket
+                + ") = " + PaintJob.getBucketCount(width, height, areaPerBucket, extraBucket) + " (expected: "
+                + expectedResult + ")");
+
+        width = 1.4;
+        height = 2.1;
+        areaPerBucket = -1.5;
+        extraBucket = 2;
+        expectedResult = -1;
+        System.out.println("getBucketCount(" + width + ", " + height + ", " + areaPerBucket + ", " + extraBucket
+                + ") = " + PaintJob.getBucketCount(width, height, areaPerBucket, extraBucket) + " (expected: "
+                + expectedResult + ")");
+
+        width = -3.4;
+        height = 2.1;
+        areaPerBucket = 1.5;
+        expectedResult = -1;
+        System.out.println("getBucketCount(" + width + ", " + height + ", " + areaPerBucket
+                + ") = " + PaintJob.getBucketCount(width, height, areaPerBucket) + " (expected: "
+                + expectedResult + ")");
+
+        width = 3.4;
+        height = 2.1;
+        areaPerBucket = 1.5;
+        expectedResult = 5;
+        System.out.println("getBucketCount(" + width + ", " + height + ", " + areaPerBucket
+                + ") = " + PaintJob.getBucketCount(width, height, areaPerBucket) + " (expected: "
+                + expectedResult + ")");
+
+        width = 7.25;
+        height = 4.3;
+        areaPerBucket = 2.35;
+        expectedResult = 14;
+        System.out.println("getBucketCount(" + width + ", " + height + ", " + areaPerBucket
+                + ") = " + PaintJob.getBucketCount(width, height, areaPerBucket) + " (expected: "
+                + expectedResult + ")");
+
+        area = 3.4;
+        areaPerBucket = 1.5;
+        expectedResult = 3;
+        System.out.println("getBucketCount(" + area + ", " + areaPerBucket
+                + ") = " + PaintJob.getBucketCount(area, areaPerBucket) + " (expected: "
+                + expectedResult + ")");
+
+        area = 6.26;
+        areaPerBucket = 2.2;
+        expectedResult = 3;
+        System.out.println("getBucketCount(" + area + ", " + areaPerBucket
+                + ") = " + PaintJob.getBucketCount(area, areaPerBucket) + " (expected: "
+                + expectedResult + ")");
+
+        area = 3.26;
+        areaPerBucket = 0.75;
+        expectedResult = 5;
+        System.out.println("getBucketCount(" + area + ", " + areaPerBucket
+                + ") = " + PaintJob.getBucketCount(area, areaPerBucket) + " (expected: "
+                + expectedResult + ")");
+    }
 }
--- src/main/java/PaintJob.java
+++ src/main/java/PaintJob.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class PaintJob {
+public class PaintJob {
+
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+        int numberOfBucketsThatBobNeedsToBuyBeforeGoingToWork = -1;
+        if (width > 0 && height > 0 && areaPerBucket > 0 && extraBuckets >= 0) {
+            double area = width * height;
+            double areaThatBobCanCover = extraBuckets * areaPerBucket;
+            if (areaThatBobCanCover >= area) {
+                numberOfBucketsThatBobNeedsToBuyBeforeGoingToWork = 0;
+            } else {
+                double remainingArea = area - areaThatBobCanCover;
+                numberOfBucketsThatBobNeedsToBuyBeforeGoingToWork = (int) Math.ceil(remainingArea / areaPerBucket);
+            }
+        }
+        return numberOfBucketsThatBobNeedsToBuyBeforeGoingToWork;
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+        int numberOfBucketsThatBobNeedsToBuyBeforeGoingToWork = -1;
+        if (width > 0 && height > 0 && areaPerBucket > 0) {
+            double area = width * height;
+            if (area == 0) {
+                numberOfBucketsThatBobNeedsToBuyBeforeGoingToWork = 0;
+            } else {
+                numberOfBucketsThatBobNeedsToBuyBeforeGoingToWork = (int) Math.ceil(area / areaPerBucket);
+            }
+        }
+        return numberOfBucketsThatBobNeedsToBuyBeforeGoingToWork;
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket) {
+        int numberOfBucketsThatBobNeedsToBuyBeforeGoingToWork = -1;
+        if (area > 0 && areaPerBucket > 0) {
+            numberOfBucketsThatBobNeedsToBuyBeforeGoingToWork = (int) Math.ceil(area / areaPerBucket);
+        }
+        return numberOfBucketsThatBobNeedsToBuyBeforeGoingToWork;
+    }
 }
